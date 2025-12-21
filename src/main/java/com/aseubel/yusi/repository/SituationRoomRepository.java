54a8a452e162f5abffaa3859bb54ebebcdee3e56
Aseubel <1228392319@qq.com>
2025-12-21 15:35:48 +0800

    fix: repository query for members search

--- src/main/java/com/aseubel/yusi/repository/SituationRoomRepository.java
+++ src/main/java/com/aseubel/yusi/repository/SituationRoomRepository.java
@@ -2,9 +2,14 @@ package com.aseubel.yusi.repository;
 
 import com.aseubel.yusi.pojo.entity.SituationRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SituationRoomRepository extends JpaRepository<SituationRoom, String> {
-    java.util.List<SituationRoom> findByMembersContainingOrderByCreatedAtDesc(String userId);
+    @Query(value = "SELECT * FROM situation_room WHERE members LIKE CONCAT('%\"', :userId, '\"%') ORDER BY created_at DESC", nativeQuery = true)
+    List<SituationRoom> findByMembersContainingOrderByCreatedAtDesc(@Param("userId") String userId);
 }
