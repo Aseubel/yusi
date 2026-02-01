5986c086e97a0456660e98476a8bfbb7f8ca645f
Aseubel <1228392319@qq.com>
2026-02-01 20:41:08 +0800

    fix: 补充重建脚本

---
+++ rebuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 步骤1：先清理
+echo "清理旧资源..."
+docker system prune -af --volumes
+docker builder prune -af
+
+# 步骤2：拉取代码
+git pull
+
+# 步骤3：Maven清理并构建（加clean）
+mvn clean compile package -Dmaven.test.skip=true -s settings.xml
+
+# 步骤4：Docker构建
+docker compose build --no-cache
+
+# 步骤5：启动服务
+docker compose up -d
+
+# 步骤6：事后清理
+docker image prune -af
