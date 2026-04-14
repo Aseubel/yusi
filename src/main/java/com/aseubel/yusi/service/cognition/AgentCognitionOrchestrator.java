b2b5fe6bd3a58b2b987036ce1fc453fc613c0a44
Aseubel <1228392319@qq.com>
2026-04-14 13:52:20 +0800

    fix(认知编排): 添加三种认知数据摄取事件处理方法
    添加日记、聊天和情绪广场认知数据摄取的事件处理方法，以支持不同类型认知数据的处理

--- src/main/java/com/aseubel/yusi/service/cognition/AgentCognitionOrchestrator.java
+++ src/main/java/com/aseubel/yusi/service/cognition/AgentCognitionOrchestrator.java
@@ -1,8 +1,17 @@
 package com.aseubel.yusi.service.cognition;
 
+import com.aseubel.yusi.common.event.ChatCognitionIngestEvent;
+import com.aseubel.yusi.common.event.DiaryCognitionIngestEvent;
+import com.aseubel.yusi.common.event.EmotionPlazaCognitionIngestEvent;
 import com.aseubel.yusi.pojo.dto.cognition.CognitionIngestCommand;
 
 public interface AgentCognitionOrchestrator {
 
     void ingest(CognitionIngestCommand command);
+
+    void onDiaryCognitionIngest(DiaryCognitionIngestEvent event);
+
+    void onChatCognitionIngest(ChatCognitionIngestEvent event);
+
+    void onEmotionPlazaCognitionIngest(EmotionPlazaCognitionIngestEvent event);
 }
